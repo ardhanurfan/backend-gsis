d7bfbad049848c7201d05d2b89ccb63e684f1d1a
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-06-21 17:57:42 +0700

    feat: BCC user

--- app/Http/Controllers/API/BccController.php
+++ app/Http/Controllers/API/BccController.php
@@ -2,10 +2,121 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\BccUser;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class BccController extends Controller
 {
-    //
+    function all(Request $request) {
+        $user_id = $request->input('user_id');
+        
+        if ($user_id){
+            $bcc_user = BccUser::with('user')->where('user_id',$user_id)->first();
+            if ($bcc_user) {
+                return ResponseFormatter::success(
+                    $bcc_user,
+                    'Data peserta berhasil diambil' 
+                );
+            }else{
+                return ResponseFormatter::error(
+                    null,
+                    'Data peserta tidak ada',
+                    404
+                );
+            }
+        }
+        $bcc_user = BccUser::with('user');
+        return ResponseFormatter::success(
+            $bcc_user->get(),
+            'Data peserta berhasil diambil' 
+        );
+    }
+
+    function register(Request $request) {
+        try {
+            $request->validate([
+            'stream'=>'required|in:ART,BUSINESS,TECHNOLOGY',
+            'ktm_url'=>'required',
+            'ss_follow_url'=>'required',
+            'ss_poster_url'=>'required',
+            'payment_url'=>'required',
+        ]);
+
+        $id = Auth::id();
+
+        $bcc_user = BccUser::create([
+            'user_id'=> $id,
+            'team_id'=>$request->team_id,
+            'papper_url'=>$request->papper_url,
+            'stream'=>$request->stream,
+            'ktm_url'=>$request->ktm_url,
+            'ss_follow_url'=>$request->ss_follow_url,
+            'ss_poster_url'=>$request->ss_poster_url,
+            'payment_url'=>$request->payment_url,
+            'approve_ktm'=>$request->approve_ktm,
+            'approve_follow'=>$request->approve_follow,
+            'approve_poster'=>$request->approve_poster,
+            'approve_payment'=>$request->approve_payment,
+        ]);
+        return ResponseFormatter::success(
+                $bcc_user,
+                'Create Bcc User successfully'
+            );
+        } catch (ValidationException $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something when wrong',
+                'error' => array_values($error->errors())[0][0],    
+            ], 
+                'Create Bcc User failed', 
+                500,
+            );
+        }
+    }
+
+    function editFromUser(Request $request) {
+        try {
+            $request->validate([
+            'ktm_url'=>'required',
+            'ss_follow_url'=>'required',
+            'ss_poster_url'=>'required',
+            'payment_url'=>'required',
+        ]);
+
+        $id = Auth::id();
+        $edit = BccUser::find($id);
+
+        if (!$edit) {
+            return ResponseFormatter::error(
+                null,
+                'Data not found',
+                404
+            );
+        }
+
+        $edit->update([
+            'ktm_url'=>$request->ktm_url,
+            'ss_follow_url'=>$request->ss_follow_url,
+            'ss_poster_url'=>$request->ss_poster_url,
+            'payment_url'=>$request->payment_url,
+        ]);
+
+        return ResponseFormatter::success(
+            $edit,
+            'Edit Bcc User success'
+        );
+
+        } catch (ValidationException $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something when wrong',
+                'error' => array_values($error->errors())[0][0],    
+            ], 
+                'Edit Bcc User failed', 
+                500,
+            );
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\BccController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
+Route::get('bcc-user', [BccController::class, 'all']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [UserController::class, 'get']);
     Route::post('logout', [UserController::class, 'logout']);
     Route::post('edit-profile', [UserController::class, 'editProfile']);
+    
+    Route::post('register-bcc-user', [BccController::class, 'register']);
+    Route::post('edit-bcc-user', [BccController::class, 'editFromUser']);
 });
