8ccb4afc43cfbe2bcae358cbfa168530ee3ee937
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-06-25 09:59:43 +0700

    Update ExhibitionController.php

--- app/Http/Controllers/API/ExhibitionController.php
+++ app/Http/Controllers/API/ExhibitionController.php
@@ -3,10 +3,36 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Exhibition;
 use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
 
 class ExhibitionController extends Controller
 {
-    
+    function all(Request $request) {
+        $user_id = $request->input('user_id');
+        
+        if ($user_id){
+            $exhibition_user = Exhibition::with('user')->where('user_id',$user_id)->first();
+            if ($exhibition_user) {
+                return ResponseFormatter::success(
+                    $exhibition_user,
+                    'Data peserta berhasil diambil' 
+                );
+            }else{
+                return ResponseFormatter::error(
+                    null,
+                    'Data peserta tidak ada',
+                    404
+                );
+            }
+        }
+        $bcc_user = BccUser::with('user');
+        return ResponseFormatter::success(
+            $bcc_user->get(),
+            'Data peserta berhasil diambil' 
+        );
+    } 
+
     //
 }
